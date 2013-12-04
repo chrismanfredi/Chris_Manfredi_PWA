abcb8d7fff2b0c363c1511ac93d843177df5ae79
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-12-04 18:12:27 -0500

    Finish Duel 2
    Finish Duel 2

--- Duel 2/js/main.js
+++ Duel 2/js/main.js
@@ -3,17 +3,20 @@
 
     console.log("FIGHT!");
 
-    // Players
-    var PlayerOne = "Spiderman";
-    var PlayerTwo = "Batman";
+    var playerone = ["Spiderman", 20, 100];
+    var playertwo = ["Batman", 20, 100];
 
-    // Health
-    var PlayerOneHealth = 100;
-    var PlayerTwoHealth = 100;
+//    Players
+//    var PlayerOne = "Spiderman";
+//    var PlayerTwo = "Batman";
 
-    // Damage
-    var PlayerOneDamage = 20;
-    var PlayerTwoDamage = 20;
+//    Health
+//    var PlayerOneHealth = 100;
+//    var PlayerTwoHealth = 100;
+//
+//    Damage
+//    var PlayerOneDamage = 20;
+//    var PlayerTwoDamage = 20;
 
     // Round
     var round = 0;
@@ -21,29 +24,29 @@
     function fight(){
         console.log('in the fight function');
 
-        alert(PlayerOne+":"+PlayerOneHealth+" Start "+PlayerTwo+":"+PlayerTwoHealth);
+        alert(playerone[0]+":"+playerone[2]+" Start "+playertwo[0]+":"+playertwo[2]);
 
         for (var j=0; j < 10; j++){
 
             // Random Formula is - Math.floor(Math.random() * (Max - Min) + Min);
 
-            var minDamage1 = PlayerOneDamage * .5;
-            var minDamage2 = PlayerTwoDamage * .5;
-            var v1 = Math.floor(Math.random()*(PlayerOneDamage-minDamage1)+minDamage1);
-            var v2 = Math.floor(Math.random()*(PlayerTwoDamage-minDamage2)+minDamage2);
+            var minDamage1 = playerone[1] * .5;
+            var minDamage2 = playertwo[1] * .5;
+            var v1 = Math.floor(Math.random()*(playerone[1]-minDamage1)+minDamage1);
+            var v2 = Math.floor(Math.random()*(playertwo[1]-minDamage2)+minDamage2);
 
            // damage
-           PlayerOneHealth-=v1;
-           PlayerTwoHealth-=v2;
+           playerone[2]-=v1;
+           playertwo[2]-=v2;
 
-           console.log(PlayerOne+":"+PlayerOneHealth+" "+PlayerTwo+":"+PlayerTwoHealth);
+           console.log(playerone+":"+playerone+" "+playertwo+":"+playertwo);
 
            var results = winnerCheck();
            console.log(results);
 
            if(results === "no winner"){
                round++;
-               alert(PlayerOne+":"+PlayerOneHealth+" Round "+round+ " Over "+PlayerTwo+":"+PlayerTwoHealth);
+               alert(playerone[0]+":"+playerone[2]+" Round "+round+ " Over "+playertwo[0]+":"+playertwo[2]);
            }else{
                alert(results);
                break;
@@ -62,12 +65,12 @@
     function winnerCheck(){
        var results="no winner";
 
-       if(PlayerOneHealth<1 && PlayerTwoHealth<1){
+       if(playerone[2]<1 && playerone[2]<1){
            results = "You Both Die";
-       } else if(PlayerOneHealth<1){
-           results = PlayerTwo+" Wins!"
-       } else if(PlayerTwoHealth<1){
-           results = PlayerOne+" Wins!"
+       } else if(playerone[2]<1){
+           results = playertwo[0]+" Wins!"
+       } else if(playertwo[2]<1){
+           results = playerone[0]+" Wins!"
        };
 
        return results;
